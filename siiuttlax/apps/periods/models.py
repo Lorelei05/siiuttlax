81241c078e048f184abe8983f292bd15510596c8
NoeNavarro1 <noenavarroacevedo92@gmail.com>
2024-07-19 10:04:46 -0600

    Changes

--- siiuttlax/apps/periods/models.py
+++ siiuttlax/apps/periods/models.py
@@ -7,10 +7,26 @@ class Period(models.Model):
         ('mayo -  agosto', 'mayo - agosto'),
         ('septiembre - diciembre', 'septiembre - diciembre')
     ]
-    period = models.CharField(max_length=25, choices=PERIODS)
-    year = models.CharField(max_length=4)
-    cicle = models.CharField(max_length=11, default='2023 - 2024')
+    period = models.CharField(max_length=25, choices=PERIODS, verbose_name='Periodo')
+    year = models.CharField(max_length=4,verbose_name='Año', default=2024)
+    cicle = models.CharField(max_length=15, verbose_name='Ciclo', default='2024 - 2025')
+    is_active =models.BooleanField(verbose_name='Activo', default=False)
+
+    def _str_(self):
+        return f'{ self.period } { self.year }'
+    class Meta:
+        verbose_name = 'Periodo'
+        verbose_name_plural = 'Periodos'
+        ordering=['id']
 
 class  Semester(models.Model):
-    semester = models.IntegerField()
-    semester_name = models.CharField(max_length=20)
+    semester = models.CharField(max_length=2, verbose_name="Cuatrimestre")
+    semester_name = models.CharField(max_length=10, verbose_name="Cuatrimestre en Letra")
+
+    def _str_(self):
+        return f'{self.semester_name}'
+    
+    class Meta:
+        verbose_name = 'Cuatrimestre'
+        verbose_name_plural = 'Cuatrimestres'
+        ordering=['id']
